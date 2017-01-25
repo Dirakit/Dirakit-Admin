5c5948f81f99f4e4e79a4a814c56276745368136
aulia faqih <aulia faqih>
2017-01-25 12:39:20 +0700

    fix custom.js

--- assets/custom.js
+++ assets/custom.js
@@ -12,16 +12,12 @@ function activate_dropdown() {
 	$(document).ready(function(){
 		$('#project_filter').click(function(){
 			$('#project_filter').dropdown();
-		});		
-	})
+		});
 
-	$(document).ready(function(){
 		$('#feed1').click(function(){
 			$('#feed1').dropdown();
-		});		
-	})
+		});
 
-	$(document).ready(function(){
 		$('#feed2').click(function(){
 			$('#feed2').dropdown();
 		});		
