1ddbeb8fbf1b2dd5dfbb84071aa85dded852f607
aulia faqih <aulia faqih>
2017-01-28 04:35:22 +0700

    fix custom.js 2

--- assets/custom.js
+++ assets/custom.js
@@ -1,25 +1,9 @@
-function activate_sidebar() {
+function activate_feature() {
 	// body...
 	$(document).ready(function(){
 		$('#menu-sidebar').click(function(){
 			$('.ui.sidebar').sidebar('toggle');
-		});		
-	})
-}
-
-function activate_dropdown() {
-	// body...
-	$(document).ready(function(){
-		$('#project_filter').click(function(){
-			$('#project_filter').dropdown();
 		});
-
-		$('#feed1').click(function(){
-			$('#feed1').dropdown();
-		});
-
-		$('#feed2').click(function(){
-			$('#feed2').dropdown();
-		});		
+		$('.dropdown').dropdown();		
 	})
 }
