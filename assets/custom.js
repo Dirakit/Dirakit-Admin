fc800e85f02ae51f41fa7ace6372d8c671cefe8f
aulia faqih <aulia faqih>
2017-01-31 12:23:03 +0700

    fix custom.js 3

--- assets/custom.js
+++ assets/custom.js
@@ -1,9 +1,3 @@
 function activate_feature() {
 	// body...
-	$(document).ready(function(){
-		$('#menu-sidebar').click(function(){
-			$('.ui.sidebar').sidebar('toggle');
-		});
-		$('.dropdown').dropdown();		
-	})
 }
