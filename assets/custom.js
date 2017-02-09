487b31944836b3de8a4e6071e409d4bc5549122c
aulia faqih <aulia faqih>
2017-02-09 14:11:10 +0700

    fix empty function

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,20 @@
-function activate_feature() {
+function sidebarController() {
 	// body...
+	$('#menu-sidebar').click(function(){
+		if ($('.ui.sidebar').sidebar('is visible')) {
+			$('.ui.sidebar').sidebar('hide');
+			$('.pusher').css('margin-left','0%');
+			$('.custom-header').css('width','100%');
+			$('.ui.sidebar').sidebar({
+				dimPage: false,
+				closable: false,
+			});
+		}
+		else if ($('.ui.sidebar').sidebar('is hidden')) {
+			$('.ui.sidebar').sidebar('setting','transition','overlay');
+			$('.ui.sidebar').sidebar('show');
+			$('.pusher').css('margin-left','20%');
+			$('.custom-header').css('width','80%');
+		}
+	});
 }
